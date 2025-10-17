af404f46fc4ca27e65c9f42ba36d4d544f567f6a
ashleyjlr <larotondaashley@gmail.com>
2025-10-17 15:50:17 -0400

    Updates dev stack name

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@
 # Users can modify these values before deployment
 
 # Stack name for the development CDK deployment
-STACK_NAME = "OneL-DV"
+STACK_NAME = "OneL-DEV"
 
 # Cognito domain name for authentication
 COGNITO_DOMAIN_NAME = "one-l-auth" 
