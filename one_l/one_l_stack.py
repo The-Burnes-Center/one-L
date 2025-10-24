ac17e370f70526482ce23c8a1612d6df9c65d9cb
NehaTummala <nehatummala02@gmail.com>
2025-10-24 14:45:42 -0400

    Fix CDK import error - remove unused constants imports

--- one_l/one_l_stack.py
+++ one_l/one_l_stack.py
@@ -4,7 +4,7 @@ from .authorization.authorization import AuthorizationConstruct
 from .agent_api.agent_api import AgentApiConstruct
 from .api_gateway.api_gateway import ApiGatewayConstruct
 from .user_interface.user_interface import UserInterfaceConstruct
-from constants import TEST_COGNITO_DOMAIN_NAME, DEV_COGNITO_DOMAIN_NAME, ONEL_TEST_COGNITO_DOMAIN_NAME
+# Removed unused imports - using environment variables instead
 
 class OneLStack(Stack):
 
