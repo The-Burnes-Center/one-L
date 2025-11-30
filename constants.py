e4aec00f4d6a8425569867c1a73dc917a8abf538
DB-25 <dhruvbaradiya@gmail.com>
2025-11-29 21:40:24 -0500

    fix: correct stack name to OneL-DV2 in constants.py

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@
 # Users can modify these values before deployment
 
 # Stack name for the production CDK deployment
-STACK_NAME = "OneL-v2"
+STACK_NAME = "OneL-DV2"
 
 # Cognito domain name for authentication
 COGNITO_DOMAIN_NAME = "onel-v2-one-l-auth"
